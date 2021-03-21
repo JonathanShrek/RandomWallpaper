da145423d88f2856aa73da55b09572f1ad276fad
Jonathan Shreckengost <jonathanshrek@gmail.com>
2021-03-20 21:57:46 -0400

    Modified to show same random wallpaper on all connected displays

--- wallpaper.sh
+++ wallpaper.sh
@@ -2,6 +2,22 @@
 
 while true
 do
-	feh --randomize --bg-scale ~/Pictures/Wallpapers/*
+    WALL=$(find "${HOME}/Pictures/Wallpapers/" -type f | sort -R | tail -1)
+    feh --no-fehbg --bg-scale "$WALL"
+
+	#screen_size=$(xrandr | grep ' connected' | awk '{print $3}' | cut -f1 -d"+")
+
+	#IFS=$'\n'
+	#readarray -t <<<"$screen_size"
+	#feh_command="feh --randomize --bg-scale"
+
+	#for i in "${MAPFILE[@]}"
+	#do
+    		#echo size: "$i"
+    		#feh_command="$feh_command ~/Pictures/Wallpapers/*"
+	#done
+
+	#eval "$feh_command"
+
 	sleep 500s;
 done &
